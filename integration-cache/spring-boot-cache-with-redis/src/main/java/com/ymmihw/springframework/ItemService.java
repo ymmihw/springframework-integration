3495da0c8bb8b3f6644a4b0a5fee88f2ce0af3c8
wanghaoming <wanghaoming@ctfo.com>
2021-05-14 17:17:51 +0800

    add log

--- integration-cache/spring-boot-cache-with-redis/src/main/java/com/ymmihw/springframework/ItemService.java
+++ integration-cache/spring-boot-cache-with-redis/src/main/java/com/ymmihw/springframework/ItemService.java
@@ -1,17 +1,20 @@
 package com.ymmihw.springframework;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
+@Slf4j
 public class ItemService {
 
     private final ItemRepository itemRepository;
 
     @Cacheable(value = "itemCache")
     public Item getItemForId(String id) {
+        log.info("call from ItemService");
         return itemRepository.findById(id)
                 .orElseThrow(RuntimeException::new);
     }
