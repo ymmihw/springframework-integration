0fcc83e86504cdc7d66ece8e1f6c9141437d02c0
wanghaoming <wanghaoming@ctfo.com>
2021-05-19 13:39:58 +0800

    add update

--- integration-cache/spring-boot-cache-with-redis/src/main/java/com/ymmihw/springframework/ItemService.java
+++ integration-cache/spring-boot-cache-with-redis/src/main/java/com/ymmihw/springframework/ItemService.java
@@ -5,6 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.util.Optional;
+
 @Service
 @AllArgsConstructor
 @Slf4j
@@ -19,4 +22,18 @@ public class ItemService {
                 .orElseThrow(RuntimeException::new);
     }
 
+    public Item saveOrUpdate(String id) {
+        Optional<Item> op = itemRepository.findById(id);
+        Item item;
+        if (op.isPresent()) {
+            item = op.get();
+            item.setDescription("" + Instant.now());
+        } else {
+            item = new Item();
+            item.setId(id);
+            item.setDescription("" + Instant.now());
+        }
+
+        return itemRepository.save(item);
+    }
 }
